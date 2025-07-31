7112b8e86a1c5c4839fb91dc9578c871029d193d
jilin0105 <1559667807@qq.com>
2025-07-31 10:17:52 +0800

    Update Footer.tsx

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useTranslation } from "react-i18next";
 import Container from "./Container";
 import { Link } from "react-router-dom";
@@ -14,48 +13,25 @@ const Footer: React.FC = () => {
           <p className="mb-2">
             © {year} {t("app.title")}
           </p>
-          <div className="flex flex-wrap justify-center items-center space-x-4 mb-2">
+          <div className="flex flex-wrap justify-center items-center space-x-4">
             <Link
               to="/privacy-policy"
               className="hover:text-primary transition-colors"
             >
               {t("common.privacyPolicy", "隐私政策")}
             </Link>
-            <Link to="/terms" className="hover:text-primary transition-colors">
+            <Link 
+              to="/terms" 
+              className="hover:text-primary transition-colors"
+            >
               {t("common.terms", "使用条款")}
             </Link>
-            <Link to="/about" className="hover:text-primary transition-colors">
+            <Link 
+              to="/about" 
+              className="hover:text-primary transition-colors"
+            >
               {t("common.about", "关于我们")}
             </Link>
-          </div>
-          <div className="flex justify-center items-center space-x-4">
-            <a
-              href="https://zaunist.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-primary transition-colors"
-            >
-              <i className="fas fa-blog mr-1"></i>
-              {t("common.blog")}
-            </a>
-            <a
-              href="https://www.youtube.com/@zaunist"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-primary transition-colors"
-            >
-              <i className="fab fa-youtube mr-1"></i>
-              {t("common.youtube")}
-            </a>
-            <a
-              href="https://xugou.mdzz.uk"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-primary transition-colors"
-            >
-              <i className="fas fa-chart-line mr-1"></i>
-              {t("common.xugouMonitor")}
-            </a>
           </div>
         </div>
       </Container>
